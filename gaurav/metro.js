88a5000f1540443b94166e2fe5d903efaa541e6f
Gaurav Joshi <gaurav@webcontentor.com>
2016-07-06 18:45:23 +0530

    underscore.js file added+in metro.js blue line of metro is completed+some small changes in index.html

--- gaurav/metro.js
+++ gaurav/metro.js
@@ -1,11 +1,64 @@
-var stationData=[{stationN0:1,name:"Vaishali",farePrice:"0rs",timeTaken:"0min"},{stationN0:2,name:"Kaushambi",farePrice:"4rs",timeTaken:"4min"},{stationN0:3,name:"Anand Vihar",farePrice:"8rs",timeTaken:"8min"},{stationN0:4,name:"Karkarduma",farePrice:"12rs",timeTaken:"12min"},{stationN0:5,name:"Preet Vihar",farePrice:"16rs",timeTaken:"16min"},{stationN0:6,name:"Nirman Vihar",farePrice:"20rs",timeTaken:"20min"},{stationN0:7,name:"Laxmi Nagar",farePrice:"24rs",timeTaken:"24min"},{stationN0:8,name:"Yamuna Bank",farePrice:"28rs",timeTaken:"28min"},{stationN0:9,name:"Indraprastha",farePrice:"32rs",timeTaken:"32min"},{stationN0:10,name:"Pragati Maidan",farePrice:"36rs",timeTaken:"36min"},{stationN0:11,name:"Mandi House",farePrice:"40rs",timeTaken:"40min"},{stationN0:12,name:"Barakhambha Road",farePrice:"44rs",timeTaken:"44min"},{stationN0:13,name:"Rajiv Chowk",farePrice:"48rs",timeTaken:"48min"},{stationN0:14,name:"Ramakrishna Ashram Marg",farePrice:"52rs",timeTaken:"52min"},{stationN0:15,name:"Jhandewalan",farePrice:"56rs",timeTaken:"56min"},{stationN0:16,name:"Karol Bagh",farePrice:"60rs",timeTaken:"60min"},{stationN0:17,name:"Rajendra Place",farePrice:"64rs",timeTaken:"64min"},{stationN0:18,name:"Patel Nagar",farePrice:"68rs",timeTaken:"68min"},{stationN0:19,name:"Shadipur",farePrice:"72rs",timeTaken:"72min"},{stationN0:20,name:"Kirti Nagar",farePrice:"76rs",timeTaken:"76min"},{stationN0:21,name:"Moti Nagar",farePrice:"80rs",timeTaken:"80min"},{stationN0:22,name:"Ramesh Nagar",farePrice:"84rs",timeTaken:"84min"},{stationN0:23,name:"Rajouri Garden",farePrice:"88rs",timeTaken:"88min"},{stationN0:24,name:"Tagore Garden",farePrice:"92rs",timeTaken:"92min"},{stationN0:25,name:"Subhash Nagar",farePrice:"96rs",timeTaken:"96min"},{stationN0:26,name:"Tilak Nagar",farePrice:"100rs",timeTaken:"100min"},{stationN0:27,name:"Janakpuri East",farePrice:"104rs",timeTaken:"104min"},{stationN0:28,name:"Janakpuri West",farePrice:"108rs",timeTaken:"108min"},{stationN0:29,name:"Uttam Nagar East",farePrice:"112rs",timeTaken:"112min"},{stationN0:30,name:"Uttam Nagar West",farePrice:"116rs",timeTaken:"116min"},{stationN0:31,name:"Nawada",farePrice:"120rs",timeTaken:"120min"},{stationN0:32,name:"Dwarka Mor",farePrice:"124rs",timeTaken:"124min"},{stationN0:33,name:"Dwarka",farePrice:"128rs",timeTaken:"128min"},{stationN0:34,name:"Dwarka Sector 14",farePrice:"132rs",timeTaken:"132min"},{stationN0:35,name:"Dwarka Sector 13",farePrice:"136rs",timeTaken:"136min"},{stationN0:36,name:"Dwarka Sector 12",farePrice:"140rs",timeTaken:"140min"},{stationN0:37,name:"Dwarka Sector 11",farePrice:"144rs",timeTaken:"144min"},{stationN0:38,name:"Dwarka Sector 10",farePrice:"148rs",timeTaken:"148min"},{stationN0:39,name:"Dwarka Sector 9",farePrice:"152rs",timeTaken:"152min"},{stationN0:40,name:"Dwarka Sector 8",farePrice:"156rs",timeTaken:"156min"},{stationN0:41,name:"Dwarka Sector 21",farePrice:"160rs",timeTaken:"160min"}]
+var stationData=[{stationN0:1,name:"Vaishali",farePrice:"0",timeTaken:"0"},{stationN0:2,name:"Kaushambi",farePrice:"4",timeTaken:"4"},{stationN0:3,name:"Anand Vihar",farePrice:"8",timeTaken:"8"},{stationN0:4,name:"Karkarduma",farePrice:"12",timeTaken:"12"},{stationN0:5,name:"Preet Vihar",farePrice:"16",timeTaken:"16"},{stationN0:6,name:"Nirman Vihar",farePrice:"20",timeTaken:"20"},{stationN0:7,name:"Laxmi Nagar",farePrice:"24",timeTaken:"24"},{stationN0:8,name:"Yamuna Bank",farePrice:"28",timeTaken:"28"},{stationN0:9,name:"Indraprastha",farePrice:"32",timeTaken:"32"},{stationN0:10,name:"Pragati Maidan",farePrice:"36",timeTaken:"36"},{stationN0:11,name:"Mandi House",farePrice:"40",timeTaken:"40"},{stationN0:12,name:"Barakhambha Road",farePrice:"44",timeTaken:"44"},{stationN0:13,name:"Rajiv Chowk",farePrice:"48",timeTaken:"48"},{stationN0:14,name:"Ramakrishna Ashram Marg",farePrice:"52",timeTaken:"52"},{stationN0:15,name:"Jhandewalan",farePrice:"56",timeTaken:"56"},{stationN0:16,name:"Karol Bagh",farePrice:"60",timeTaken:"60"},{stationN0:17,name:"Rajendra Place",farePrice:"64",timeTaken:"64"},{stationN0:18,name:"Patel Nagar",farePrice:"68",timeTaken:"68"},{stationN0:19,name:"Shadipur",farePrice:"72",timeTaken:"72"},{stationN0:20,name:"Kirti Nagar",farePrice:"76",timeTaken:"76"},{stationN0:21,name:"Moti Nagar",farePrice:"80",timeTaken:"80"},{stationN0:22,name:"Ramesh Nagar",farePrice:"84",timeTaken:"84"},{stationN0:23,name:"Rajouri Garden",farePrice:"88",timeTaken:"88"},{stationN0:24,name:"Tagore Garden",farePrice:"92",timeTaken:"92"},{stationN0:25,name:"Subhash Nagar",farePrice:"96",timeTaken:"96"},{stationN0:26,name:"Tilak Nagar",farePrice:"100",timeTaken:"100"},{stationN0:27,name:"Janakpuri East",farePrice:"104",timeTaken:"104"},{stationN0:28,name:"Janakpuri West",farePrice:"108",timeTaken:"108"},{stationN0:29,name:"Uttam Nagar East",farePrice:"112",timeTaken:"112"},{stationN0:30,name:"Uttam Nagar West",farePrice:"116",timeTaken:"116"},{stationN0:31,name:"Nawada",farePrice:"120",timeTaken:"120"},{stationN0:32,name:"Dwarka Mor",farePrice:"124",timeTaken:"124"},{stationN0:33,name:"Dwarka",farePrice:"128",timeTaken:"128"},{stationN0:34,name:"Dwarka Sector 14",farePrice:"132",timeTaken:"132"},{stationN0:35,name:"Dwarka Sector 13",farePrice:"136",timeTaken:"136"},{stationN0:36,name:"Dwarka Sector 12",farePrice:"140",timeTaken:"140"},{stationN0:37,name:"Dwarka Sector 11",farePrice:"144",timeTaken:"144"},{stationN0:38,name:"Dwarka Sector 10",farePrice:"148",timeTaken:"148"},{stationN0:39,name:"Dwarka Sector 9",farePrice:"152",timeTaken:"152"},{stationN0:40,name:"Dwarka Sector 8",farePrice:"156",timeTaken:"156"},{stationN0:41,name:"Dwarka Sector 21",farePrice:"160",timeTaken:"160"}]
+
+var startStation;
+var endStation;
+var obj1;
+var obj2;
+var station1;
+var station2;
+var stationDifference;
+var timetaken1;
+var timetaken2;
+var timeDifference;
+var farePrice1;
+var farePrice2;
+var farePriceDiffrence;
 
 $(document).ready(function () {
+
 	$("#Map").hide();
 	$("#extraDetail").hide();
+
 	$("#fullMap").click(function () {
 		$("#Map").show();
-	});	
+	});
+
+	$("#reset").click(function () {
+		$('#boardingStation').val('');
+		$('#deboardingStation').val('');
+		$("#fareprice").text("0");
+		$("#noofstation").text("0");
+		$("#timetaken").text("0");
+	});
+
 	$("#search").click(function () {
-		$("#extraDetail").show();	
+		$("#extraDetail").show();
+		getStation();
+		getDetail();
+		$("#fareprice").text(farePriceDiffrence);
+		$("#noofstation").text(stationDifference);
+		$("#timetaken").text(timeDifference);
+	});	
+
+	function getStation() {
+		var e1 = document.getElementById("boardingStation");
+		startStation = e1.options[e1.selectedIndex].text;
+        obj1 = _.find(stationData, function(obj1) { return obj1.name == startStation });
+
+		var e2 = document.getElementById("deboardingStation");
+		endStation = e2.options[e2.selectedIndex].text;
+        obj2 = _.find(stationData, function(obj2) { return obj2.name == endStation });
+	}
+
+	function getDetail() {
+		station1 = obj1.stationN0;
+		station2 = obj2.stationN0;
+		stationDifference = Math.abs(station2 - station1);
+		timetaken1 = obj1.timeTaken;
+		timetaken2 = obj2.timeTaken;
+		timeDifference = Math.abs(timetaken2 - timetaken1);
+		farePrice1 = obj1.farePrice;
+		farePrice2 = obj2.farePrice;
+		farePriceDiffrence = Math.abs(farePrice2 - farePrice1);
+	}
 });
