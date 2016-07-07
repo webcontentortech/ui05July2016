cdd4dd9c1e641c97587e3649b69e8c8285f650d3
vivek sengar <vivek@webcontentor.com>
2016-07-07 18:29:27 +0530

    changes have been made but still need to be further modified

--- vivek/game.js
+++ vivek/game.js
@@ -1,39 +1,62 @@
+
+
+    var imgArray = [];
 $( document ).ready(function() {
+	imgArray[0] = new Image();
+	imgArray[0].src = "1";
 
+	$('#check').html(imgArray[0]);
 
+    
+	console.log(imgArray[0]);
+	imgArray[1] = new Image();
+	imgArray[1].src = "2";
+	imgArray[2] = new Image();
+	imgArray[2].src = "3";
+	console.log(imgArray[2].src);
+	imgArray[3] = new Image();
+	imgArray[3].src = "4";
+	imgArray[4] = new Image();
+	imgArray[4].src = "5";
 
-$(".b").animate({marginTop: "487px"},9000,"linear");
-document.onkeydown = function(e) {
-switch (e.keyCode) {
-case 37:
-moveLeft();
-break;
-case 38:
-moveup();
-break;
-case 39:
-moveRight();
-break;
-case 40:
-movedown();
-break;
-}
-};
-function moveRight(){
-$(".b").animate().stop();
-$(".b").animate({'marginLeft' : "385px"},5000, "linear"
-)};
-function moveLeft(){
-$(".b").animate().stop();
-$(".b").animate({'marginLeft' : "-385px"},5000, "linear"
-// body...
-)};
-function movedown(){
-$(".b").animate().stop();
-$(".b").animate({'marginTop' : "485px"},2000, "linear"
-// body...
-)};
+	
+	for (var i = 0; i <= 5; i++) {
+		var x = imgArray[i].src;
+		console.log(x);
+		$("#1").animate({marginTop: "466px"},900,"linear");
+		console.log(x)
+	
+	}
 
+	document.onkeydown = function(e) {
+		switch (e.keyCode) {
+		case 37:
+		moveLeft();
+		break;
+		case 38:
+		moveup();
+		break;
+		case 39:
+		moveRight();
+		break;
+		case 40:
+		movedown();
+		break;
+		}
+	}
+	function moveRight(){
+		$("#1").animate().stop();
+		$("#1").animate({'marginLeft' : "365px"},5000, "linear");
+	}
 
+	function moveLeft(){
+		$("#1").animate().stop();
+		$("#1").animate({'marginLeft' : "-365px"},5000, "linear");
+	}
 
+	function movedown(){
+		$("#1").animate().stop();
+		$("#1").animate({'marginTop' : "466px"},2000, "linear");
+	}
 });
+
