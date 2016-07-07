8883debeba6b323ba80ea94301644a7eb6bce66a
Vidyadhar Bhatt <vidyadhar@webcontentor.com>
2016-07-07 20:05:47 +0530

    changes done

--- vidyadhar/v.js
+++ vidyadhar/v.js
@@ -1,4 +1,4 @@
-var v=[["what is the color of blood?","red","blue","A"],["what is the color of mango?","red","yellow","B"],["what is the color of apple?","red","blue","A"],["what is the color of banana?","yellow","blue","A"],["what is the color of orange?","orange","blue","A"],["what is the color of grapes?","yellow","green","B"],["what is the color of cherry?","yellow","red","B"]]
+var v=[["what is the color of blood?","red","blue","A"],["what is the color of mangoes?","red","yellow","B"],["what is the color of apple?","red","blue","A"],["what is the color of banana?","yellow","blue","A"],["what is the color of orange?","orange","blue","A"],["what is the color of grapes?","yellow","green","B"],["what is the color of cherry?","yellow","red","B"]]
 
 $(document).ready(function(){
     $("#start").click(function() {
