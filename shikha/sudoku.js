0e92e1f042d5f8979d47c0a6ab9fc9410d594bef
Shikha Choubey <shikha@webcontentor.com>
2016-07-08 18:29:57 +0530

    completed looping

--- shikha/sudoku.js
+++ shikha/sudoku.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var clock;
-
+    submit();
     var solved =[
                 [2,7,6,3,1,4,9,5,8],
                 [8,5,4,9,6,2,7,1,3],
@@ -12,154 +12,127 @@ $(document).ready(function(){
                 [6,4,1,2,5,3,8,7,9],
                 [7,8,9,6,4,1,5,3,2],
                 ];
-    var unsolved =  [
-                    [2,,,3,,,,,],
-                    [8,,4,,6,2,,,3],
-                    [,1,3,8,,,2,,],
-                    [,,,,2,,3,9,],
-                    [5,,7,,,,6,2,1],
-                    [,3,2,,,6,,,],
-                    [,2,,,,9,1,4,0],
-                    [6,,1,2,5,,8,,9],
-                    [,,,,,1,,,2],
-                    ];
-
-   
-    $("#01").text(unsolved[0][0]);
-    var a=$("#02").val();
-
-    $("#03").val(unsolved[0][2]);
-    $("#04").text(unsolved[0][3]);
-    $("#05").val(unsolved[0][4]);
-    $("#06").val(unsolved[0][5]);
-    $("#07").val(unsolved[0][6]);
-    $("#08").val(unsolved[0][7]);
-    $("#09").val(unsolved[0][8]);
-
-    $("#11").text(unsolved[1][0]);
-    $("#12").val(unsolved[1][1]);
-    $("#13").text(unsolved[1][2]);
-    $("#14").val(unsolved[1][3]);
-    $("#15").text(unsolved[1][4]);
-    $("#16").text(unsolved[1][5]);
-    $("#17").val(unsolved[1][6]);
-    $("#18").val(unsolved[1][7]);
-    $("#19").text(unsolved[1][8]);
-
-    $("#21").val(unsolved[2][0]);
-    $("#22").text(unsolved[2][1]);
-    $("#23").text(unsolved[2][2]);
-    $("#24").text(unsolved[2][3]);
-    $("#25").val(unsolved[2][4]);
-    $("#26").val(unsolved[2][5]);
-    $("#27").text(unsolved[2][6]);
-    $("#28").val(unsolved[2][7]);
-    $("#29").val(unsolved[2][8]);
-
-    $("#31").val(unsolved[3][0]);
-    $("#32").val(unsolved[3][1]);
-    $("#33").val(unsolved[3][2]);
-    $("#34").val(unsolved[3][3]);
-    $("#35").text(unsolved[3][4]);
-    $("#36").val(unsolved[3][5]);
-    $("#37").text(unsolved[3][6]);
-    $("#38").text(unsolved[3][7]);
-    $("#39").val(unsolved[3][8]); 
-
-    $("#41").text(unsolved[4][0]);
-    $("#42").val(unsolved[4][1]);
-    $("#43").text(unsolved[4][2]);
-    $("#44").val(unsolved[4][3]);
-    $("#45").val(unsolved[4][4]);
-    $("#46").val(unsolved[4][5]);
-    $("#47").text(unsolved[4][6]);
-    $("#48").text(unsolved[4][7]);
-    $("#49").text(unsolved[4][8]);
-
-    $("#51").val(unsolved[5][0]);
-    $("#52").text(unsolved[5][1]);
-    $("#53").text(unsolved[5][2]);
-    $("#54").val(unsolved[5][3]);
-    $("#55").val(unsolved[5][4]);
-    $("#56").text(unsolved[5][5]);
-    $("#57").val(unsolved[5][6]);
-    $("#58").val(unsolved[5][7]);
-    $("#59").val(unsolved[5][8]); 
-
-    $("#61").val(unsolved[6][0]); 
-    $("#62").text(unsolved[6][1]);
-    $("#63").val(unsolved[6][2]);
-    $("#64").val(unsolved[6][3]);
-    $("#65").val(unsolved[6][4]);
-    $("#66").text(unsolved[6][5]);
-    $("#67").text(unsolved[6][6]);
-    $("#68").text(unsolved[6][7]);
-    $("#69").val(unsolved[6][8]); 
-
-    $("#71").text(unsolved[7][0]);
-    $("#72").val(unsolved[7][1]);
-    $("#73").text(unsolved[7][2]);
-    $("#74").text(unsolved[7][3]);
-    $("#75").text(unsolved[7][4]);
-    $("#76").val(unsolved[7][5]);
-    $("#77").text(unsolved[7][6]);
-    $("#78").val(unsolved[7][7]);
-    $("#79").text(unsolved[7][8]);
-
-    $("#81").val(unsolved[8][0]);
-    $("#82").val(unsolved[8][1]);
-    $("#83").val(unsolved[8][2]);
-    $("#84").val(unsolved[8][3]);
-    $("#85").val(unsolved[8][4]);
-    $("#86").text(unsolved[8][5]);
-    $("#87").val(unsolved[8][6]);
-    $("#88").val(unsolved[8][7]);
-    $("#89").text(unsolved[8][8]);
+    
+    $("#01").text(solved[0][0]);
+    $("#04").text(solved[0][3]);
+    $("#11").text(solved[1][0]);
+    $("#13").text(solved[1][2]);
+    $("#15").text(solved[1][4]);
+    $("#16").text(solved[1][5]);
+    $("#19").text(solved[1][8]);
+    $("#22").text(solved[2][1]);
+    $("#23").text(solved[2][2]);
+    $("#24").text(solved[2][3]);
+    $("#27").text(solved[2][6]);
+    $("#35").text(solved[3][4]);
+    $("#37").text(solved[3][6]);
+    $("#38").text(solved[3][7]);
+    $("#41").text(solved[4][0]);
+    $("#43").text(solved[4][2]);
+    $("#47").text(solved[4][6]);
+    $("#48").text(solved[4][7]);
+    $("#49").text(solved[4][8]);
+    $("#52").text(solved[5][1]);
+    $("#53").text(solved[5][2]);
+    $("#56").text(solved[5][5]);
+    $("#62").text(solved[6][1]);
+    $("#66").text(solved[6][5]);
+    $("#67").text(solved[6][6]);
+    $("#68").text(solved[6][7]);
+    $("#71").text(solved[7][0]);
+    $("#73").text(solved[7][2]);
+    $("#74").text(solved[7][3]);
+    $("#75").text(solved[7][4]);
+    $("#77").text(solved[7][6]);
+    $("#79").text(solved[7][8]);
+    $("#86").text(solved[8][5]);
+    $("#89").text(solved[8][8]);
 
     $("input").on("input",function () {
         var cell = $(this).val();
         if ((cell.length > 0) && !$.isNumeric(cell)) {
             alert('Please input only numbers from 1 - 9');
             $(this).val("");
-            console.log(cell);
         }
     });
+
     $("#start").click(function(){
-        clock=setTimeout( "$('#table').hide();",24000 );
+        clock=setTimeout( "$('#table').hide();",240000 );
         console.log(clock);
         alert("time out play again");
-
     });
 
     $("#pause").click(function(){
         var pause= $("#start").stop();
         console.log(pause);
     });
+
     function submit() {
         $("#submit").click(function(){
-            console.log(a);
-            if(unsolved[0][1]==solved[0][1] && unsolved[0][2]==solved[0][2] && unsolved[0][4]==solved[0][4]
-                && unsolved[0][5]==solved[0][5] && unsolved[0][6]==solved[0][6] && unsolved[0][7]==solved[0][7]
-                && unsolved[1][1]==solved[1][1] && unsolved[1][3]==solved[1][3] && unsolved[1][6]==solved[1][6] 
-                && unsolved[1][7]==solved[1][7] && unsolved[2][0]==solved[2][0] && unsolved[2][4]==solved[2][4] 
-                && unsolved[2][5]==solved[2][5] && unsolved[2][7]==solved[2][7] && unsolved[2][8]==solved[2][8]
-                && unsolved[3][0]==solved[3][0] && unsolved[3][1]==solved[3][1] && unsolved[3][2]==solved[3][2]
-                && unsolved[3][3]==solved[3][3] && unsolved[3][5]==solved[3][5] && unsolved[3][8]==solved[3][8]
-                && unsolved[4][1]==solved[4][1] && unsolved[4][3]==solved[4][3] && unsolved[4][4]==solved[4][4]
-                && unsolved[4][5]==solved[4][5] && unsolved[5][0]==solved[5][0] && unsolved[5][3]==solved[5][3]
-                && unsolved[5][4]==solved[5][4] && unsolved[5][6]==solved[5][6] && unsolved[5][7]==solved[5][7]
-                && unsolved[5][8]==solved[5][8] && unsolved[6][0]==solved[6][0] && unsolved[6][2]==solved[6][2]
-                && unsolved[6][3]==solved[6][3] && unsolved[6][4]==solved[6][4] && unsolved[6][8]==solved[6][8]
-                && unsolved[7][1]==solved[7][1] && unsolved[7][5]==solved[7][5] && unsolved[7][7]==solved[7][7]
-                && unsolved[8][0]==solved[8][0] && unsolved[8][1]==solved[8][1] && unsolved[8][2]==solved[8][2]
-                && unsolved[8][3]==solved[8][3] && unsolved[8][4]==solved[8][4] && unsolved[8][6]==solved[8][6]
-                && unsolved[8][7]==solved[8][7]){
-            
-                alert("u win game");
-         
+            var t1=$("#02").val();
+            var t2=$("#03").val();
+            var t3=$("#05").val();
+            var t4=$("#06").val();
+            var t5=$("#07").val();
+            var t6=$("#08").val();
+            var t7=$("#09").val();
+            var t8=$("#12").val();
+            var t9=$("#14").val();
+            var t10=$("#17").val();
+            var t11=$("#18").val();
+            var t12=$("#21").val();
+            var t13=$("#25").val();
+            var t14=$("#26").val();
+            var t15=$("#28").val();
+            var t16=$("#29").val();
+            var t17=$("#31").val();
+            var t18=$("#32").val();
+            var t19=$("#33").val();
+            var t20=$("#34").val();
+            var t21=$("#36").val();
+            var t22=$("#39").val();
+            var t23=$("#42").val(); 
+            var t24=$("#44").val();
+            var t25=$("#45").val();
+            var t26=$("#46").val();
+            var t27=$("#51").val();
+            var t28=$("#54").val();
+            var t29=$("#55").val();
+            var t30=$("#57").val();
+            var t31=$("#58").val();
+            var t32=$("#59").val();
+            var t33=$("#61").val(); 
+            var t34=$("#63").val();
+            var t35=$("#64").val();
+            var t36=$("#65").val();
+            var t37=$("#69").val(); 
+            var t38=$("#72").val();
+            var t39=$("#76").val();
+            var t40=$("#78").val();
+            var t41=$("#81").val();
+            var t42=$("#82").val();
+            var t43=$("#83").val();
+            var t44=$("#84").val();
+            var t45=$("#85").val();
+            var t46=$("#87").val();
+            var t47=$("#88").val();
+
+            if(solved[0][1]==t1 && solved[0][2]==t2 && solved[0][4]==t3 && solved[0][5]==t4 &&
+               solved[0][6]==t5 && solved[0][8]==t6 && solved[0][7]==t7 && solved[1][1]==t8 && 
+               solved[1][3]==t9 && solved[1][6]==t10 && solved[1][7]==t11 && solved[2][0]==t12 
+               && solved[2][4]==t13 && solved[2][5]==t14 && solved[2][7]==t15 && solved[2][8]==t16
+               && solved[3][0]==t17 && solved[3][1]==t18 && solved[3][2]==t19 && solved[3][3]==t20
+               && solved[3][5]==t21 && solved[3][8]==t22 && solved[4][1]==t23 && solved[4][3]==t24 
+               && solved[4][4]==t25 && solved[4][5]==t26 && solved[5][0]==t27 && solved[5][3]==t28
+               && solved[5][4]==t29 && solved[5][6]==t30 && solved[5][7]==t31 && solved[5][8]==t32
+               && solved[6][0]==t33 && solved[6][2]==t34 && solved[6][3]==t35 && solved[6][4]==t36
+               && solved[6][8]==t37 && solved[7][1]==t38 && solved[7][5]==t39 && solved[7][7]==t40 
+               && solved[8][0]==t41 && solved[8][1]==t42 && solved[8][2]==t43 && solved[8][3]==t44 
+               && solved[8][4]==t45 && solved[8][6]==t46 && solved[8][7]==t47){
+               alert("u win game");
             }else{
-                alert("u lost game");
-            }
+               alert("u lost game");
+            }                                                                                      
         });
     }
 });
