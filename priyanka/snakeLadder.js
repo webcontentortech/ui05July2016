7cdc070595702482cc36e558733fd7fc4001758b
Priyanka Gupta <priyanka@webcontentor.com>
2016-07-08 18:29:31 +0530

    added the functionality of two flag

--- priyanka/snakeLadder.js
+++ priyanka/snakeLadder.js
@@ -6,7 +6,12 @@ $(document).ready(function(){
     var firstPlayer = 1;
     var secondPlayer = 1;
     var chance = true;
+    var firstBySnake = false;
+    var secondbySnake = false;
+    var count = 0;
     var snake = ["7","31","37","39", "55", "64", "87","98"];
+    var ladder = [ "17","43", "61","70"];
+
     $("#start").click(function(){
         $("#dice").show();
         $("#diceValue").show();
@@ -39,35 +44,36 @@ $(document).ready(function(){
         $("#1").removeClass("addGreen"); 
         for (var i = 0; i < snake.length; i++) {
             if (snake[i]==firstPlayer) {
-                console.log("snake bites me");
                 firstNotMove();
             }else{
                 console.log("ha ha ha safe");
             }
-        }
+        }  
+        if (firstBySnake==false) {
             firstPlayer = firstPlayer + dice;
-            console.log("in move method:"+firstPlayer);
             $("#" + firstPlayer).addClass("addGreen");
+        }else{
+            firstPlayer = count + dice;
+        }  
     }
 
     var secondPlayerMove = function(){
         $("#1").removeClass("addYellow");
         for (var i = 0; i < snake.length; i++) {
             if (snake[i]==secondPlayer) {
-                console.log("snake bites me");
                 secondNotMove();
             }else{
                 console.log("ha ha ha safe");
             }
         } 
+        if (secondbySnake==false) {
         secondPlayer = secondPlayer + dice;
         $("#" + secondPlayer).addClass("addYellow");
+        }
     }
 
     var firstNotMove = function(){
-        //console.log("in not move method:"+firstPlayer);
-        firstPlayer = firstPlayer - dice;
-       // console.log("after minus:" + firstPlayer);
+        count = firstPlayer - dice;
     }
     
     var secondNotMove = function(){
